c15dd4f109a5f1bef9523c6ea92bb71527639918
D.Galga <ex.spake@gmail.com>
2024-01-09 20:03:06 -0600

    added input system and settings, beginnings of player controller.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float speed = 5.0f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
