25fc82cd9c6711709bd6eac810661527337bbf04
D.Galga <ex.spake@gmail.com>
2024-01-16 23:47:23 -0600

    ui rotation.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,13 +9,20 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D _rb;
     Vector2 _moveDirection;
 
+    Transform _moveShootUI;
+
     void Start(){
         _rb = GetComponent<Rigidbody2D>();
+        _moveShootUI = transform.GetChild(0);
     }
 
     void FixedUpdate()
     {
         _rb.velocity = _moveDirection * speed;
+        if(_moveDirection != Vector2.zero){
+            float angle = Mathf.Atan2(_moveDirection.y, _moveDirection.x) * Mathf.Rad2Deg;
+            _moveShootUI.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     void OnMove(InputValue val) {
